dfaed4d72be8afdab44eca5b266c7d189f105879
parker8889 <1295028204@qq.com>
2025-09-24 20:57:33 +0800

    Update test1.c

--- test1.c
+++ test1.c
@@ -1,11 +1,13 @@
 #include<stdio.h>
 #include<string.h>
 int main(){
+    //定义1个叫input的数组，容量为10
     char input[10];
+    //构建了一个无限循环体。分别处理输入Dian，Quit，以及其他情况
     while(1){
         scanf("%s",input);
-        if(strcmp(input,"Dian")==0)
-           { printf("2002\n");} 
+        if(strcmp(input,"Dian")==0 )
+        { printf("2002\n");} 
        else if (strcmp(input,"Quit")==0)
        {break;}
        else 
